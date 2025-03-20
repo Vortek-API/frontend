9962cfc091844480367f1ec3ae1eefd0061c39cc
Thiago <teagos371@gmail.com>
2025-03-20 01:01:27 -0300

    feat: (VTK-27) adicionado empresas-button.component

---
+++ src/app/components/empresas/empresas-button/empresas-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empresas-button',
+  standalone: false,
+  templateUrl: './empresas-button.component.html',
+  styleUrl: './empresas-button.component.css'
+})
+export class EmpresasButtonComponent {
+
+}
