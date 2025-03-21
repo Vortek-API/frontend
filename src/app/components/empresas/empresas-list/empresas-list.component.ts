d9aef5c33aa713f5089c6efada89046ddcd7ceba
Thiago <teagos371@gmail.com>
2025-03-20 21:47:00 -0300

    feat: (VTK-27) criado empresas-list.component

---
+++ src/app/components/empresas/empresas-list/empresas-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empresas-list',
+  standalone: false,
+  templateUrl: './empresas-list.component.html',
+  styleUrl: './empresas-list.component.css'
+})
+export class EmpresasListComponent {
+
+}
