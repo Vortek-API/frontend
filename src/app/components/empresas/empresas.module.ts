61b3dcbbfbde2f2a1d3c515719fd3d9512ccda88
Thiago <teagos371@gmail.com>
2025-03-20 21:47:51 -0300

    feat: (VTK-27) add empresas-list no empresas.module

--- src/app/components/empresas/empresas.module.ts
+++ src/app/components/empresas/empresas.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EmpresasButtonComponent } from './empresas-button/empresas-button.component';
+import { EmpresasListComponent } from './empresas-list/empresas-list.component';
 
 
 
 @NgModule({
   declarations: [
-    EmpresasButtonComponent
+    EmpresasButtonComponent,
+    EmpresasListComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    EmpresasButtonComponent
+    EmpresasButtonComponent,
+    EmpresasListComponent
   ]
 })
 export class EmpresasModule { }
