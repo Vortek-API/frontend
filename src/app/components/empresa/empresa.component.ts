9918a67add184cc01410333cad810f7335328616
Thiago <teagos371@gmail.com>
2025-03-28 23:50:19 -0300

    Revert "feat: (VTK-28) Componente para editar/deletar e adicionar empresa"
    This reverts commit 0ab455da1833682b2631546757f12f2340411e8a.

--- src/app/components/empresa/empresa.component.ts
+++ src/app/components/empresa/empresa.component.ts
@@ -1,80 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { EmpresaService } from './empresa.service';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-empresa',
-  standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [],
   templateUrl: './empresa.component.html',
-  styleUrls: ['./empresa.component.css']
+  styleUrl: './empresa.component.css'
 })
-export class EmpresaComponent implements OnInit {
-  empresas: any[] = [];
-  empresaForm!: FormGroup;
-  mensagem = '';
-  editando = false;
-  idEditando: number | null = null;
+export class EmpresaComponent {
 
-  constructor(private empresaService: EmpresaService, private fb: FormBuilder) {}
-
-  ngOnInit(): void {
-    this.listarEmpresas();
-    this.empresaForm = this.fb.group({
-      nome: ['', Validators.required],
-      cnpj: ['', [Validators.required, Validators.pattern(/^\d{14}$/)]],
-      telefone: ['', Validators.required],
-      foto: ['']
-    });
-  }
-
-  listarEmpresas() {
-    this.empresaService.listarEmpresas().subscribe(empresas => {
-      this.empresas = empresas;
-    });
-  }
-
-  salvarEmpresa() {
-    if (this.empresaForm.invalid) {
-      this.mensagem = 'Preencha todos os campos obrigatórios!';
-      return;
-    }
-
-    const empresa = this.empresaForm.value;
-    if (this.editando && this.idEditando !== null) {
-      this.empresaService.editarEmpresa(this.idEditando, empresa).subscribe(() => {
-        this.mensagem = 'Empresa editada com sucesso!';
-        this.listarEmpresas();
-        this.cancelarEdicao();
-      });
-    } else {
-      this.empresaService.adicionarEmpresa(empresa).subscribe(() => {
-        this.mensagem = 'Empresa adicionada com sucesso!';
-        this.listarEmpresas();
-      });
-    }
-
-    this.empresaForm.reset();
-  }
-
-  editar(empresa: any) {
-    this.editando = true;
-    this.idEditando = empresa.id;
-    this.empresaForm.patchValue(empresa);
-  }
-
-  deletar(id: number) {
-    this.empresaService.deletarEmpresa(id).subscribe(() => {
-      this.mensagem = 'Empresa excluída com sucesso!';
-      this.listarEmpresas();
-    });
-  }
-
-  cancelarEdicao() {
-    this.editando = false;
-    this.idEditando = null;
-    this.empresaForm.reset();
-  }
 }
