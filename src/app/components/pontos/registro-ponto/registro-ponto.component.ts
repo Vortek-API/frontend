586d1856c3e9757654e8db8b04a304d21e166c91
Thiago <162724918+mariinetic@users.noreply.github.com>
2025-04-23 22:12:04 -0300

    feat (VTK-64): Criado tela de pontos (parcial)

---
+++ src/app/components/pontos/registro-ponto/registro-ponto.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-registro-ponto',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './registro-ponto.component.html',
+  styleUrl: './registro-ponto.component.css'
+})
+export class RegistroPontoComponent {
+  
+
+}
