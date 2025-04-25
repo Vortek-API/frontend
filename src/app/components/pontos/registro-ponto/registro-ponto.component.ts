45f30edb7626fcebdcdb9b43c2eadfd10d7f4d12
Thiago <162724918+mariinetic@users.noreply.github.com>
2025-04-25 15:59:01 -0300

    feat (VTK-67): Adicionado visualização de registro de pontos com filtros

--- src/app/components/pontos/registro-ponto/registro-ponto.component.ts
+++ src/app/components/pontos/registro-ponto/registro-ponto.component.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { PontoDetalhado, RegistroPontoService } from './registro-ponto.service';
+import { Empresa, EmpresaService } from '../../empresa/empresa.service';
+import { Colaborador, ColaboradorService } from '../../colaborador/colaborador.service';
 
 @Component({
   selector: 'app-registro-ponto',
@@ -9,7 +12,68 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './registro-ponto.component.html',
   styleUrl: './registro-ponto.component.css'
 })
-export class RegistroPontoComponent {
-  
 
+export class RegistroPontoComponent implements OnInit {
+  registros: PontoDetalhado[] = [];
+  colaboradores: Colaborador[] = [];
+  empresas: Empresa[] = [];
+  searchTerm: string = '';
+  selectedDate: string | null = null;
+  selectedEmpresa: number | null = null;
+
+  constructor(
+    private pontoService: RegistroPontoService,
+    private empresaService: EmpresaService,
+    private colaboradorService: ColaboradorService
+  ) { }
+
+  async ngOnInit() {
+    await this.loadEmpresas();
+    await this.loadColaboradores();
+    await this.loadRegistros();
+  }
+
+  async loadRegistros() {
+    this.registros = await this.pontoService.findAll();
+  }
+
+  async loadColaboradores() {
+    this.colaboradores = await this.colaboradorService.findAll();
+  }
+
+  async loadEmpresas() {
+    this.empresas = await this.empresaService.findAll();
+  }
+
+  get registrosFiltrados() {
+    return this.registros.filter(r => {
+      const colaborador = this.colaboradores.find(c => c.id === r.colaboradorId);
+      if (!colaborador) return false;
+
+      const nome = colaborador.nome?.toLowerCase() || '';
+      const cpf = colaborador.cpf || '';
+      const termoBusca = this.searchTerm.toLowerCase();
+
+      const registroData = r.data?.split('T')[0];
+      const atendeBusca = this.searchTerm === '' || nome.includes(termoBusca) || cpf.includes(termoBusca);
+      const atendeData = !this.selectedDate || registroData === this.selectedDate;
+      const atendeEmpresa = !this.selectedEmpresa || r.empresaId === +this.selectedEmpresa;
+
+      return atendeBusca && atendeData && atendeEmpresa;
+    });
+  }
+
+
+
+  getNomeColaborador(id: number) {
+    return this.colaboradores.find(c => c.id === id)?.nome || 'Desconhecido';
+  }
+
+  getCpfColaborador(id: number) {
+    return this.colaboradores.find(c => c.id === id)?.cpf || '---';
+  }
+
+  getNomeEmpresa(id: number) {
+    return this.empresas.find(e => e.id === id)?.nome || '---';
+  }
 }
---
+++ src/app/components/pontos/registro-ponto/registro-ponto.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { firstValueFrom, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { Colaborador, ColaboradorService } from '../../colaborador/colaborador.service';
+
+export interface PontoDetalhado {
+    id: number;
+    colaboradorId: number;
+    empresaId: number;
+    data: string;
+    horaEntrada: string;
+    horaSaida: string;
+    tempoTotal: string;
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+export class RegistroPontoService {
+
+    private apiUrl = `${environment.apiUrl}/ponto`
+
+    constructor(private http: HttpClient,
+        private colaboradorService: ColaboradorService
+    ) { }
+
+    async findAll(): Promise<PontoDetalhado[]> {
+        return firstValueFrom(this.http.get<PontoDetalhado[]>(`${this.apiUrl}/detalhado`));
+    }
+    async find(id: number): Promise<PontoDetalhado> {
+        return firstValueFrom(this.http.get<PontoDetalhado>(`${this.apiUrl}/${id}/detalhado`));
+    }
+}
