41e826a030519d9cded3ad1a75c3153f80e5eeeb
Luiz Ricardo <lu.iz.ricardo@hotmail.com>
2025-05-23 22:55:04 -0300

    fix(VTK-119): Inclusão de paginação na tela de empresas e ateração de cor na seleção de paginas.

--- src/app/components/empresa/empresa.component.ts
+++ src/app/components/empresa/empresa.component.ts
@@ -5,59 +5,52 @@ import { ModalCadastroComponent } from './modais/modal-cadastro/modal-cadastro.c
 import { MatDialog } from '@angular/material/dialog';
 import { ModalEditarDeletarComponent } from './modais/modal-editar-deletar/modal-editar-deletar.component';
 import { FormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
+
 @Component({
   selector: 'app-empresa',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, NgxPaginationModule],
   templateUrl: './empresa.component.html',
   styleUrls: ['./empresa.component.css']
 })
 export class EmpresaComponent implements OnInit {
   empresas: Empresa[] = [];
   searchTerm: string = '';
-  ordenacaoSelecionada: string | null = null;
-  selectedDate: string | null = null;
   ordenacao: string | null = null;
+  selectedDate: string | null = null;
 
+  itensPorPagina = 20;
+  paginaAtual = 1;
 
   constructor(
     private empresaService: EmpresaService,
     public dialog: MatDialog
   ) { }
 
-  async ngOnInit() {
+  ngOnInit(): void {
     this.loadEmpresas();
   }
 
-  openModalCadastro() {
-    this.dialog.open(ModalCadastroComponent, {
-    });
-
-    this.dialog.afterAllClosed.subscribe(async () => {
-      await this.loadEmpresas();
-      // setTimeout(async () => {
-      //   await this.loadEmpresas();
-      // }, 2000);
-    });
-  }
-  openModalEditar() {
-    this.dialog.open(ModalEditarDeletarComponent, {
-    });
-
-    this.dialog.afterAllClosed.subscribe(async () => {
-      await this.loadEmpresas();
-      // setTimeout(async () => {
-      //   await this.loadEmpresas();
-      // }, 2000);
-    });
-  }
   async loadEmpresas() {
     this.empresas = await this.empresaService.findAll();
   }
+
+  openModalCadastro() {
+    this.dialog.open(ModalCadastroComponent, {});
+    this.dialog.afterAllClosed.subscribe(() => this.loadEmpresas());
+  }
+
+  openModalEditar() {
+    this.dialog.open(ModalEditarDeletarComponent, {});
+    this.dialog.afterAllClosed.subscribe(() => this.loadEmpresas());
+  }
+
   clickRow(empresa: Empresa) {
     this.empresaService.setData(empresa, this.empresas);
     this.openModalEditar();
   }
+
   get empresasFiltradas(): Empresa[] {
     let filtradas = this.empresas.filter(empresa =>
       empresa.nome.toLowerCase().includes(this.searchTerm.toLowerCase())
@@ -69,19 +62,21 @@ export class EmpresaComponent implements OnInit {
 
     if (this.ordenacao === 'cadastro' && this.selectedDate) {
       filtradas = filtradas.filter(empresa => {
-        const dataEmpresa = empresa.dataCadastro?.split('T')[0]; // pega só "2025-04-14"
+        const dataEmpresa = empresa.dataCadastro?.split('T')[0];
         return dataEmpresa === this.selectedDate;
       });
     }
+
     return filtradas;
   }
+
   handleOrdenacaoChange() {
-    // Resetar data se saiu da ordenação por cadastro
+    this.paginaAtual = 1;
+
     if (this.ordenacao !== 'cadastro') {
       this.selectedDate = null;
     }
 
-    // Se limpou os filtros
     if (!this.ordenacao) {
       this.selectedDate = null;
       this.searchTerm = '';
